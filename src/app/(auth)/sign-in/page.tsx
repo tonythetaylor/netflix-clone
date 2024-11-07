2f0ca7d9b2ee788724e5d66aac33c00d9a4b1015
Anthony Taylor <ant.taylor2@gmail.com>
2024-11-07 15:51:19 -0500

    first commit, created login/register

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Input from "@/components/input";
+import Image from "next/image";
+import { useCallback, useState } from "react";
+
+const SignInPage = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+  return (
+    <div className="relative h-full w-full bg-[url('/hero.jpg')] bg-no-repeat bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-4 ">
+          <Image src={"/logo.png"} alt="logo" width={150} height={20} />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(ev: any) => setName(ev.target.value)}
+                  id="name"
+                  type="name"
+                  value={name}
+                />
+              )}
+              <Input
+                label="Email"
+                onChange={(ev: any) => setEmail(ev.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={(ev: any) => setPassword(ev.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white w-full rounded-md mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Sign Up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === 'login' ? 'First time using nextflix?' : 'Already have an account?'}
+              <span
+                onClick={toggleVariant}
+                className="text-white mk-1 hover:underline cursor-pointer"
+              >
+                {" "}
+                {variant === 'login' ? 'Create an account' : 'Login'}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
